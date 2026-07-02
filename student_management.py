043fbae0b0c7fff22a3af2b5527e1e2a8cf9f162
tejaswiananthapalli <tejaswiananthapalli@gmail.com>
2026-07-02 20:39:46 +0530

    Update student_management.py

--- student_management.py
+++ student_management.py
@@ -1,1 +1,15 @@
-print("Welcome to Student Management System")
+print("....Student Management System....")
+while True:
+  print("\n1. Add Student")
+  print("2.View Student")
+  print("3.Exit")
+  choice=int(input("enter your choice: "))
+  if choice==1:
+    print("Students will be added soon")
+  elif choice==2:
+    print("view Student feature will bw added soon")
+  elif choice==3:
+    print("Thank you")
+  else:
+    print("Invalid choice")
+    
